a77cf510d4060d5fca1ec199761207675706527e
Jordan Na <jordster02@gmail.com>
2024-07-09 23:09:31 -0400

    corrected url

--- js/modules/ajax/leaderboard-services.mjs
+++ js/modules/ajax/leaderboard-services.mjs
@@ -1,6 +1,6 @@
 export const leaderboardServices = (() => {
    const getLeaderboard = async () => {
-      const response = await fetch("../../php/scores.php/leaderboard");
+      const response = await fetch("http://localhost:8000/server.php/leaderboard");
       try {
          const data = await response.json();
          return data;
