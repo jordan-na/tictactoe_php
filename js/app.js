174ee8bf479acfe04cc47a01a2fd69efe7a162df
Jordan Na <jordster02@gmail.com>
2024-07-09 23:08:19 -0400

    set up score initialization with ajas

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
+import { scoreServices } from "./modules/ajax/score-services.mjs";
 import { eventHandler } from "./modules/event-handler.mjs";
+import { uiController } from "./modules/ui-controller.mjs";
 
-const init = () => {
+const init = async () => {
    eventHandler.setupEventListeners();
+   try {
+      const scores = await scoreServices.getScores();
+      uiController.updateScores(scores);
+   } catch (error) {
+      console.error("Error:", error);
+   }
 };
 
 window.onload = init;
