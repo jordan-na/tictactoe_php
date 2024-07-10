8d0929250aa8a7b025431d8813a09af2e0f93863
Jordan Na <jordster02@gmail.com>
2024-07-09 23:09:48 -0400

    added score ajax services

--- js/modules/ajax/score-services.mjs
+++ js/modules/ajax/score-services.mjs
@@ -1,22 +1,35 @@
 export const scoreServices = (() => {
-   const incrementScore = (player) => {
-      fetch("../../php/scores.php/scores", {
-         method: "POST",
-         headers: {
-            "Content-Type": "application/json",
-         },
-         body: JSON.stringify({ player: player }),
-      })
-      .then((response) => response.json())
-      .then((data) => {
-         console.log("Score updated:", data);
-      })
-      .catch((error) => console.error("Error:", error));
-   }
+   const incrementScore = async (player) => {
+      try {
+         const response = await fetch("http://localhost:8000/server.php/scores", {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ player: player }),
+         });
+         const data = await response.json();
+         return data;
+      } catch (error) {
+         console.error("Error:", error);
+      }
+   };
 
    const getScores = async () => {
-      const response = await fetch("../../php/scores.php/scores");
+      const response = await fetch("http://localhost:8000/server.php/scores");
       try {
+         const data = await response.json();
+         return data;
+      } catch (error) {
+         console.error("Error:", error);
+      }
+   };
+
+   const resetScores = async () => {
+      try {
+         const response = await fetch("http://localhost:8000/server.php/reset", {
+            method: "POST",
+         });
          const data = await response.json();
          return data;
       } catch (error) {
@@ -27,5 +40,6 @@ export const scoreServices = (() => {
    return {
       incrementScore: incrementScore,
       getScores: getScores,
+      resetScores: resetScores
    };
 })();
